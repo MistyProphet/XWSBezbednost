d24e7a2ebdc30329662dc33279787c7cdf465c8c
BojanaZ <bojana.zoranovic@hotmail.com>
2015-06-18 04:59:03 +0200

    Bezbednost. Osnovne klase za potpisivanje dokumenata.

---
+++ XMLProject/src/com/project/document/crypto/sig/CLRList.java
@@ -0,0 +1,18 @@
+package com.project.document.crypto.sig;
+
+import java.security.cert.Certificate;
+
+public class CLRList {
+	
+	public static boolean checkCertificate(Certificate cert) {
+
+		//ucitati fajl
+		
+		//proveriti potpis fajla
+		
+		//
+		
+		
+		return false;
+	}
+}

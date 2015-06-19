706c4804b403dca70c2a07cd6a499dfc5219824e
Zieghailo <isakov.m@gmail.com>
2015-06-19 12:44:03 +0200

    Login now working. Maybe remove GET login?

--- Firma/xws/src/rs/ac/uns/ftn/xws/services/users/UserService.java
+++ Firma/xws/src/rs/ac/uns/ftn/xws/services/users/UserService.java
@@ -10,12 +10,15 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
 
 import org.apache.log4j.Logger;
 
 import rs.ac.uns.ftn.xws.entities.user.User;
 import rs.ac.uns.ftn.xws.sessionbeans.users.UserDaoLocal;
+import rs.ac.uns.ftn.xws.util.ServiceException;
 
 @Path("/user")
 public class UserService {
@@ -52,18 +55,21 @@ public class UserService {
         return user;
     }
 
-//    @GET
-//    @Path("{username}")
-//    @Produces(MediaType.APPLICATION_XML)
-//    public User findByName(@PathParam("username") String username) {
-//        List<User> users = null;
-//        try {
-//            users = userDao.findBy("/user/username", true);
-//        } catch (Exception e) {
-//            log.error(e.getMessage(), e);
-//        }
-//        return users.get(0);
-//    }
+    @GET
+    @Path("{username}")
+    @Produces(MediaType.APPLICATION_XML)
+    public User findByName(@PathParam("username") String username) {
+        List<User> users = null;
+        try {
+            users = userDao.findAll("user");
+            for (User user : users)
+                if (user.getUsername().equals(username))
+                    return user;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
+    }
 	
 	@POST
 	@Path("")
@@ -79,39 +85,39 @@ public class UserService {
 		return "ok";
 	}
 	
-//	@GET 
-//    @Path("login")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public User loginGet(@QueryParam("username") String username, @QueryParam("password") String password) {
-//    	User user = null;
-//		try {
-//        	user = userDao.login(username, password);
-//        } catch (Exception e) {
-//        	log.error(e.getMessage(), e);
-//        }
-//		if(user==null){
-//			throw new ServiceException("Wrong username or password", Status.FORBIDDEN);
-//		}
-//		log.info("USER: "+user);
-//    	return user;
-//    }
+	@GET 
+    @Path("login")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User loginGet(@QueryParam("username") String username, @QueryParam("password") String password) {
+    	User user = null;
+		try {
+        	user = userDao.login(username, password);
+        } catch (Exception e) {
+        	log.error(e.getMessage(), e);
+        }
+		if(user==null){
+			throw new ServiceException("Wrong username or password", Status.FORBIDDEN);
+		}
+		log.info("USER: "+user);
+    	return user;
+    }
     
-//	@POST
-//    @Path("login")
-//    @Produces(MediaType.TEXT_HTML)
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    public String login(User sentUser) {
-//    	User user = null;
-//		try {
-//        	user = userDao.login(sentUser.getUsername(), sentUser.getPassword());
-//        } catch (Exception e) {
-//        	log.error(e.getMessage(), e);
-//        }
-//		if(user==null){//ako ne uspe prijava posalje se greska 403 - znam sta hoces, ali ti ne dozvoljavam 
-//			throw new ServiceException("Wrong username or password", Status.FORBIDDEN);
-//		}
-//    	return "ok";
-//    }
+	@POST
+    @Path("login")
+    @Produces(MediaType.TEXT_HTML)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String login(User sentUser) {
+    	User user = null;
+		try {
+        	user = userDao.login(sentUser.getUsername(), sentUser.getPassword());
+        } catch (Exception e) {
+        	log.error(e.getMessage(), e);
+        }
+		if(user==null) {
+			throw new ServiceException("Wrong username or password", Status.FORBIDDEN);
+		}
+    	return "ok";
+    }
 	
 //	@GET
 //    @Path("logout")
--- Firma/xws/src/rs/ac/uns/ftn/xws/sessionbeans/users/UserDao.java
+++ Firma/xws/src/rs/ac/uns/ftn/xws/sessionbeans/users/UserDao.java
@@ -2,10 +2,13 @@ package rs.ac.uns.ftn.xws.sessionbeans.users;
 
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.management.Query;
+import javax.ws.rs.core.Context;
+import javax.servlet.http.HttpServletRequest;
 
 import rs.ac.uns.ftn.xws.entities.user.User;
 import rs.ac.uns.ftn.xws.sessionbeans.common.GenericDao;
@@ -13,6 +16,9 @@ import rs.ac.uns.ftn.xws.sessionbeans.common.GenericDao;
 @Stateless
 @Local(UserDaoLocal.class)
 public class UserDao extends GenericDao<User, Long> implements UserDaoLocal{
+    
+    @Context
+	private HttpServletRequest request;
 
 	public static final String contextPath = "rs.ac.uns.ftn.xws.entities.user";
 	
@@ -24,10 +30,24 @@ public class UserDao extends GenericDao<User, Long> implements UserDaoLocal{
 
 	@Override
 	public User login(String username, String password) {
+        //TODO find the user by querying the database. This is suicide
+        
+        try {
+            List<User> users = this.findAll("user");
+            for (User user : users)
+                if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                    request.getSession().setAttribute("user", user);
+                    return user;
+                }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
 		return null;
 	}
 
 	@Override
 	public void logout() {
+        request.getSession().invalidate();
 	}
 }
